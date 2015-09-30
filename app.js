4e5332b3944a3caa214280b40072d9f299132c65
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-09-30 01:52:13 -0400

    index

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/index', function(req, res){
+app.get('/', function(req, res){
   res.sendFile('./public/index.html');
 });
 
