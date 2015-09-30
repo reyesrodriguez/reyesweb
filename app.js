aff74d4f87f6bfe670af504aa5f7b102d5504833
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-09-29 23:09:21 -0400

    web reyes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', function(req, res){
-    res.send("Hello World");
-})
+  res.sendFile('./public/index.html');
+});
+
 
 var server = app.listen(3000, function(){
     console.log("Server is listening on port 3000");
