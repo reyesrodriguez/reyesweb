a597e408d8baea41c7227a96a8b6e86e5888ff6e
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-10-05 18:12:11 -0400

    body

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
 
+$('#blog').click(function()
+{
+	alert("This Blog is currently under construction!")
+});
+
 var options = [
     {selector: '#visualize', offset: 100, callback: 'Materialize.fadeInImage("#visualize")' }
   ];
   Materialize.scrollFire(options);
 
 });
+
+
+document.getElementById('time').innerHTML= Date();
      
