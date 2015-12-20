1f978dcebc6bf85c4362bec1afbe931f2d250a9d
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-12-19 20:29:10 -0500

    new proj

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,9 +4,28 @@ $(document).ready(function(){
  	menuWidth: 200,
  	edge: 'left',
  	closeOnClick: true
- }
+ });
 
-);
+ $(".js").on('click', function() {
+   $(".circles-wrp").fadeIn();
+});
+
+var myCircle = Circles.create({
+  id:           'js',
+  radius:       60,
+  value:        43,
+  maxValue:     100,
+  width:        10,
+  text:         function(value){return value + '%';},
+  colors:       ['#D3B6C6', '#4B253A'],
+  duration:     400,
+  wrpClass:     'circles-wrp',
+  textClass:    'circles-text'
+  styleWrapper: true,
+  styleText:    true
+})
+
+
 
 });
 
