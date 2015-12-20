f10001caadf50cb199df1c6d25e165ee79455105
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-12-19 20:39:04 -0500

    np

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,24 +6,6 @@ $(document).ready(function(){
  	closeOnClick: true
  });
 
- $(".js").on('click', function() {
-   $(".circles-wrp").fadeIn();
-});
-
-var myCircle = Circles.create({
-  id:           'js',
-  radius:       60,
-  value:        43,
-  maxValue:     100,
-  width:        10,
-  text:         function(value){return value + '%';},
-  colors:       ['#D3B6C6', '#4B253A'],
-  duration:     400,
-  wrpClass:     'circles-wrp',
-  textClass:    'circles-text'
-  styleWrapper: true,
-  styleText:    true
-})
 
 
 
